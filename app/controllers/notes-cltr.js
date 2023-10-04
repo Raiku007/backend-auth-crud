679fca2ce7ed3892abc40e2322a8f2d451746f6c
Raiku <subhankardey1122@gmail.com>
2023-10-04 13:47:37 +0530

    Update notes-cltr.js

--- app/controllers/notes-cltr.js
+++ app/controllers/notes-cltr.js
@@ -1,32 +1,83 @@
-const Note = require('../models/note-model')
-const _ = require('lodash')
-const { validationResult } = require('express-validator')
-const notesCltr = {}
+const Note = require('../models/note-model');
+const { validationResult } = require('express-validator');
+const _ = require('lodash'); 
 
-notesCltr.list = async (req, res) => {
+const notesCtrl = {};
+
+notesCtrl.list = async (req, res) => {
     try {
-        const notes = await Note.find({ userId: req.userId })
-        res.json(notes)
-    } catch(e) {
-        res.json(e)
+        const notes = await Note.find({ userId: req.userId });
+        res.status(200).json(notes);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-}
+};
 
-notesCltr.create = async (req, res) => {
+notesCtrl.create = async (req, res) => {
     try {
-        const errors = validationResult(req)
-        if(!errors.isEmpty()) {
-            res.status(400).json({ errors: errors.array })
-        } else {
-            const body = _.pick(req.body, ['title', 'body'])
-            const note = new Note(body)
-            note.userId = req.userId
-            await note.save()
-            res.json(note)
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
-    } catch(e) {
-        res.json(e)
-    }
-}
 
-module.exports = notesCltr
+        const body = _.pick(req.body, ['title', 'body']);
+
+        const note = new Note({ ...body, userId: req.userId });
+        await note.save();
+        res.status(201).json({ message: 'Note created successfully', note });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+notesCtrl.show = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const note = await Note.findOne({ _id: id, userId: req.userId });
+        if (!note) {
+            return res.status(404).json({ error: 'Note not found' });
+        }
+        res.status(200).json(note);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+notesCtrl.update = async (req, res) => {
+    const id = req.params.id;
+    const { title, body } = req.body;
+    try {
+        const note = await Note.findOneAndUpdate(
+            { _id: id, userId: req.userId },
+            { title, body },
+            { new: true }
+        );
+        if (!note) {
+            return res.status(404).json({ error: 'Note not found' });
+        }
+        res.status(200).json({ message: 'Note updated successfully', note });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+notesCtrl.destroy = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const note = await Note.findOneAndDelete({ _id: id, userId: req.userId });
+        if (!note) {
+            return res.status(404).json({ error: 'Note not found' });
+        }
+        res.status(200).json({ message: 'Note deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+
+module.exports = notesCtrl;
