6a56a3c1f774d4001a19e619411d2744736707f4
Raiku <subhankardey1122@gmail.com>
2023-10-04 13:52:34 +0530

    Update note-model.js

--- app/models/note-model.js
+++ app/models/note-model.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
-const { Schema, model } = mongoose
+const { Schema, model } = mongoose  
 
 const noteSchema = new Schema({
-    title: String,
-    body: String,
+    title: String, 
+    body: String, 
     userId: {
         type: Schema.Types.ObjectId,
         ref: 'User'
     }
 }, { timestamps: true })
 
-const Note = model('Note', noteSchema)
-module.exports = Note
+const Note = model('Note', noteSchema) 
+module.exports = Note 
