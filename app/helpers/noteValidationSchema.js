597513f1a5c2bd7bb89c4b8c3440a646cf742392
Raiku <subhankardey1122@gmail.com>
2023-10-04 13:49:00 +0530

    Update noteValidationSchema.js

--- app/helpers/noteValidationSchema.js
+++ app/helpers/noteValidationSchema.js
@@ -1,9 +1,24 @@
+const { body } = require('express-validator');
+
 const noteValidationSchema = {
     title: {
         notEmpty: {
-            errorMessage: 'title is required'
+            errorMessage: 'Title is required'
+        },
+        isLength: {
+            options: { max: 100 },
+            errorMessage: 'Title should not exceed 100 characters'
+        }
+    },
+    body: {
+        notEmpty: {
+            errorMessage: 'Body is required'
+        },
+        isLength: {
+            options: { max: 255 },
+            errorMessage: 'Body should not exceed 255 characters'
         }
     }
-}
+};
 
-module.exports = noteValidationSchema
+module.exports = noteValidationSchema;
