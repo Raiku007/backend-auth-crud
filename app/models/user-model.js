076bb386a48d918e940b933b6c1d2b33dcbd5082
Raiku <subhankardey1122@gmail.com>
2023-10-04 13:53:01 +0530

    Update user-model.js

--- app/models/user-model.js
+++ app/models/user-model.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
-const { Schema, model } = mongoose
-// userSchema = username, email, password
-
+const { Schema, model } = mongoose 
 const userSchema = new Schema({
     username: String,
     email: String,
@@ -10,26 +8,4 @@ const userSchema = new Schema({
 
 const User = model('User', userSchema)
 
-module.exports = User
-
-// 3 types of validation
-// client side, express validation, mongoose validation
-// Databases: strictly type, JavaScript: Loosely type
-
-
-// mongoose validation 
-// When we have multiple property definition for a field
-
-// username: {
-//     type: String
-//     required: true
-// },
-// email: {
-//     type: String
-//     required: true
-// },
-// password: {
-//     type: String,
-//     required: true,
-//     minlength: 8
-// }
+module.exports = User 
