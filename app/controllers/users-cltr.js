7aaff8d6b625d757c4b5e8ecdbe968f112352b9d
Raiku <subhankardey1122@gmail.com>
2023-10-04 13:48:31 +0530

    Update users-cltr.js

--- app/controllers/users-cltr.js
+++ app/controllers/users-cltr.js
@@ -1,70 +1,77 @@
-const { validationResult } = require('express-validator')
-const User = require('../models/user-model')
-const _ = require('lodash')
-const bcryptjs = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const usersCltr = {}
+const { validationResult } = require('express-validator');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+const User = require('../models/user-model');
+const usersCtrl = {};
 
-// register
-
-usersCltr.register = async function(req, res) {
-    try {
-        const errors = validationResult(req)
-        if(!errors.isEmpty()) {
-            res.status(400).json({ errors: errors.array() })
-        } else {
-            const body = _.pick(req.body, ['username', 'email', 'password'])
-            const user = new User(body)
-            const salt = await bcryptjs.genSalt()
-            const hashedPassword = await bcryptjs.hash(user.password, salt)
-            user.password = hashedPassword 
-            const userDoc = await user.save()
-            res.json(userDoc)
-        }
-    } catch (e) {
-        res.json(e)
+// Handle user registration
+usersCtrl.register = async function (req, res) {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ success: false, errors: errors.array() });
     }
-}
 
-// login
+    const { username, email, password } = _.pick(req.body, ['username', 'email', 'password']);
+    const salt = await bcryptjs.genSalt();
+    const hashedPassword = await bcryptjs.hash(password, salt);
+    
+    const user = new User({ username, email, password: hashedPassword });
+    const userDoc = await user.save();
 
-usersCltr.login = async (req, res) => {
-    try {
-        const errors = validationResult(req)
-        if(!errors.isEmpty()) {
-            res.status(400).json({ errors: errors.array() })
-        } else {
-            const body = _.pick(req.body, ['email', 'password'])
-            const user = await User.findOne({ email: body.email })
-            if(user) {
-                const result = await bcryptjs.compare(body.password, user.password)
-                if(result) {
-                    // jwt token
-                    //res.json(user)
-                    const token = jwt.sign({ 
-                        id: user._id
-                     }, process.env.JWT_SECRET, { expiresIn: '7d' })
-                     res.json({ token: token })
-                } else {
-                    res.status(404).json({ error: 'invalid email / password' })
-                }
-            } else {
-                res.status(404).json({ error: 'invalid email / password' })
-            }
-        }
-    } catch (e) {
-        res.json(e)
+    res.json({ success: true, data: userDoc });
+  } catch (error) {
+    console.error('Error registering user:', error);
+    res.status(500).json({ success: false, error: 'An error occurred while registering the user.' });
+  }
+};
+
+// Handle user login
+usersCtrl.login = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ success: false, errors: errors.array() });
     }
-}
 
-usersCltr.account = async (req, res) => {
-    try {
-        const user = await User.findById(req.userId)
-        const resBody = _.pick(user, ['id', 'username', 'email'])
-        res.json(resBody)
-    } catch(e) {
-        res.json(e)
+    const { email, password } = _.pick(req.body, ['email', 'password']);
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ success: false, error: 'Invalid email or password' });
     }
-}
 
-module.exports = usersCltr
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(404).json({ success: false, error: 'Invalid email or password' });
+    }
+
+    const tokenData = { id: user._id };
+    const token = jwt.sign(tokenData, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.error('Error logging in:', error);
+    res.status(500).json({ success: false, error: 'An error occurred while logging in.' });
+  }
+};
+
+// Retrieve user account
+usersCtrl.account = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({ success: false, error: 'User not found' });
+    }
+
+    const userData = _.pick(user, ['_id', 'username', 'email']);
+    res.json({ success: true, data: userData });
+  } catch (error) {
+    console.error('Error retrieving user account:', error);
+    res.status(500).json({ success: false, error: 'An error occurred while retrieving the user account.' });
+  }
+};
+
+module.exports = usersCtrl;
