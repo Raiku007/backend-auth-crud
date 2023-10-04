537914ef92cd1c1f794a886404e2dc725861bd23
Raiku <subhankardey1122@gmail.com>
2023-10-04 13:54:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,33 +5,29 @@ const { checkSchema } = require('express-validator')
 
 const configureDB = require('./config/db')
 const routes = require('./config/routes')
-const usersCltr = require('./app/controllers/users-cltr')
-const notesCltr = require('./app/controllers/notes-cltr')
-
+const usersCtrl = require('./app/controllers/users-controller')
+const notesCtrl = require('./app/controllers/notes-controller')
 const { userRegistrationSchema, userLoginSchema } = require('./app/helpers/userValidationSchema')
 const noteValidationSchema = require('./app/helpers/noteValidationSchema')
 const authenticateUser = require('./app/middlewares/authenticateUser')
 const app = express() 
 const port = process.env.PORT || 3030
 
-
 configureDB()
 app.use(express.json())
 app.use(cors()) 
 
-// application level middleware  
+app.post('/api/users/register', checkSchema(userRegistrationSchema), usersCtrl.register)
+app.post('/api/users/login', checkSchema(userLoginSchema), usersCtrl.login)
+app.get('/api/users/account', authenticateUser, usersCtrl.account)
 
-// app.httpMethod(url, callback)
-// routing level middleware
-app.post('/api/users/register', checkSchema(userRegistrationSchema), usersCltr.register)
-app.post('/api/users/login', checkSchema(userLoginSchema), usersCltr.login)
-app.get('/api/users/account', authenticateUser, usersCltr.account)
-
-app.get('/api/notes', authenticateUser, notesCltr.list)
-app.post('/api/notes', authenticateUser, checkSchema(noteValidationSchema), notesCltr.create)
+app.get('/api/notes', authenticateUser, notesCtrl.list)
+app.post('/api/notes', authenticateUser, checkSchema(noteValidationSchema), notesCtrl.create)
+app.get('/api/notes/:id', authenticateUser, notesCtrl.show )
+app.put('/api/notes/:id', authenticateUser, checkSchema(noteValidationSchema), notesCtrl.update)
+app.delete('/api/notes/:id', authenticateUser, notesCtrl.destroy )
 
 app.use('/',routes)
-
 
 app.listen(port, () => {
     console.log('server running on port', port)
